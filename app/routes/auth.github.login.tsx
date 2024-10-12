71a641c19c6133f58edf1c026e9c91f133c6154d
laszloekovacs <laszloekovacs@gmail.com>
2024-10-12 16:45:02 +0200

    added vscode launch config

--- app/routes/auth.github.login.tsx
+++ app/routes/auth.github.login.tsx
@@ -2,7 +2,7 @@ import { ActionFunctionArgs, redirect } from '@remix-run/node'
 import { authenticator } from 'services/auth.server'
 
 export const loader = () => {
-	return redirect('/login')
+	return redirect('/')
 }
 
 export const action = ({ request }: ActionFunctionArgs) => {
