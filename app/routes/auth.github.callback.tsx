6332c9663b6225ea0ee014c8aee428563f15dee8
Laszlo E Kovacs <laszloekovacs@gmail.com>
2024-10-16 21:30:04 +0200

    updated

--- app/routes/auth.github.callback.tsx
+++ app/routes/auth.github.callback.tsx
@@ -3,6 +3,9 @@ import { authenticator } from '~/services/auth.server'
 
 // this is called by github oauth
 export const loader = async ({ request }: LoaderFunctionArgs) => {
+	const formData = await request.formData()
+	console.log(Object.fromEntries(formData))
+
 	return authenticator.authenticate('github', request, {
 		successRedirect: '/onboarding',
 		failureRedirect: '/'
