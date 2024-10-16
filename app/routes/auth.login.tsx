bb5b2eb41f54cb72adaf8367075c7a0a4e6a6b51
Laszlo E Kovacs <laszloekovacs@gmail.com>
2024-10-16 19:03:21 +0200

    updated

--- app/routes/auth.login.tsx
+++ app/routes/auth.login.tsx
@@ -1,8 +1,12 @@
 import { ActionFunctionArgs, json } from '@remix-run/node'
-import { Form } from '@remix-run/react'
+import { Form, useActionData } from '@remix-run/react'
 import invariant from 'tiny-invariant'
+import { z, ZodError } from 'zod'
+import { authenticator } from '~/services/auth.server'
 
 export default function LoginPage() {
+	const actionData = useActionData<typeof action>()
+
 	return (
 		<div className='grid place-content-center min-h-lvh'>
 			<img src='/logo-light.png' alt='logo' width='128'></img>
@@ -12,6 +16,9 @@ export default function LoginPage() {
 				<div className='flex flex-col gap-2 max-w-56'>
 					<label htmlFor='email'>Email</label>
 					<input type='email' name='email' id='email' required />
+					{actionData?.status == 'invalid_email' && (
+						<p className='text-red-500'>invalid email</p>
+					)}
 
 					<label htmlFor='password'>Jelszo</label>
 					<input type='password' name='password' id='password' required />
@@ -32,21 +39,40 @@ export default function LoginPage() {
 }
 
 export const action = async ({ request }: ActionFunctionArgs) => {
-	// check what action to do
-	const formData = await request.formData()
+	try {
+		// check what action to do
+		const formData = await request.formData()
+		const action = formData.get('action') as string
+		invariant(action, 'action is required')
 
-	const action = formData.get('action') as string
-	invariant(action, 'action is required')
+		switch (action) {
+			case 'login_email':
+				const emailSchema = z.string().email('hibas email forma')
+				const email = emailSchema.parse(formData.get('email'))
 
-	switch (action) {
-		case 'login_email': {
-			const email = formData.get('email') as string
-			const password = formData.get('password') as string
+				const passwordSchema = z
+					.string()
+					.min(8, 'jelszo rovidebb mint 8 karakter')
+				const password = passwordSchema.parse(formData.get('password'))
 
+				return authenticator.authenticate('form', request, {
+					successRedirect: '/',
+					failureRedirect: '/auth.login'
+				})
+
+			default:
+				throw new Error('invalid action')
+		}
+	} catch (error) {
+		if (error instanceof ZodError) {
 			return json({
-				message: 'ok',
-				status: 'ok'
+				message: error.message,
+				status: 'error'
 			})
 		}
+		// not a schema validation error, propagate
+		throw error
 	}
+
+	throw new Error('invalid action')
 }
