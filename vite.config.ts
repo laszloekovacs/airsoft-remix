228c3fa11d078c3f1309d3359f785c49c660fafd
Laszlo E Kovacs <laszloekovacs@gmail.com>
2024-12-26 15:08:35 +0100

    update Vite configuration and add .env to .gitignore

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,17 @@
-import { reactRouter } from "@react-router/dev/vite";
-import autoprefixer from "autoprefixer";
-import tailwindcss from "tailwindcss";
-import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
+import { reactRouter } from '@react-router/dev/vite'
+import autoprefixer from 'autoprefixer'
+import tailwindcss from 'tailwindcss'
+import { defineConfig } from 'vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-  css: {
-    postcss: {
-      plugins: [tailwindcss, autoprefixer],
-    },
-  },
-  plugins: [reactRouter(), tsconfigPaths()],
-});
+	css: {
+		postcss: {
+			plugins: [tailwindcss, autoprefixer]
+		}
+	},
+	plugins: [reactRouter(), tsconfigPaths()],
+	server: {
+		port: 3000
+	}
+})
