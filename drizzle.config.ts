ddc55da638dd0a6abbb1fb2f24219ee35d6167dc
Laszlo E Kovacs <laszloekovacs@gmail.com>
2024-12-26 15:40:40 +0100

    added better auth integration

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'drizzle-kit'
 
 export default defineConfig({
 	out: './drizzle',
-	schema: './app/db/schema.ts',
+	schema: ['./app/schema/index.ts', './app/schema/auth-schema.ts'],
 	dialect: 'postgresql',
 	dbCredentials: {
 		url: process.env.DATABASE_URL!
