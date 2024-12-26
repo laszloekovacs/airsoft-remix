572dc5af72290036ada5fcc666975ca7dc7c1bd3
Laszlo E Kovacs <laszloekovacs@gmail.com>
2024-12-26 15:10:46 +0100

    removed template

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,12 @@
-import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import type { Route } from './+types/home'
 
 export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
+	return [
+		{ title: 'New React Router App' },
+		{ name: 'description', content: 'Welcome to React Router!' }
+	]
 }
 
 export default function Home() {
-  return <Welcome />;
+	return null
 }
