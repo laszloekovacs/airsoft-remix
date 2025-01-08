f76eb8f16bd72c93e7a7e57d0bd9c0293f54280d
laszloekovacs <laszloekovacs@gmail.com>
2025-01-08 12:01:26 +0100

    feat: implement event loader and enhance EventPage with dynamic data display

--- app/routes/_home.event.$id.tsx
+++ app/routes/_home.event.$id.tsx
@@ -1,7 +1,37 @@
 import React from 'react'
+import type { Route } from './+types/_home.event.$id'
+import { Link, useNavigate } from 'react-router'
 
-const EventPage = () => {
-	return <div>EventPage</div>
+export const loader = async () => {
+	const eventData = {
+		title: 'Event Title',
+		description: 'Event Description',
+		date: '1982.04.13',
+		location: 'Event Location',
+		organizer: 'Peter',
+		imgurl: 'http://www.picsum.photos/200/300'
+	}
+
+	return eventData
+}
+
+const EventPage = ({ loaderData }: Route.ComponentProps) => {
+	const { title, description, date, organizer, imgurl } = loaderData
+	const navigate = useNavigate()
+
+	return (
+		<div>
+			<button onClick={() => navigate(-1)}>vissza</button>
+
+			<p>{date}</p>
+			<h2>{title}</h2>
+			<h3>szervező: {organizer}</h3>
+			<img src={imgurl} alt='Event Image' />
+			<div>
+				<div>{description}</div>
+			</div>
+		</div>
+	)
 }
 
 export default EventPage
