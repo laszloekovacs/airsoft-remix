6a9a3aa57a82adf0025411df9634833b3dedd5a8
laszloekovacs <laszloekovacs@gmail.com>
2025-01-11 19:09:04 +0100

    added auth schemas

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'drizzle-kit'
 
 export default defineConfig({
 	out: './drizzle',
-	schema: ['./app/schema/index.ts'],
+	schema: ['./app/schema/index.ts', './app/schema/auth-schema.ts'],
 	dialect: 'sqlite',
 	dbCredentials: {
 		url: process.env.DB_FILE_NAME!
