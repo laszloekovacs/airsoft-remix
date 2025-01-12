f1bacd051b6c2b87389359ac060201c2d58a5e86
laszloekovacs <laszloekovacs@gmail.com>
2025-01-12 19:16:06 +0100

    updated

--- app/routes/_home.user.group.$id_.post.tsx
+++ app/routes/_home.user.group.$id_.post.tsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import { Form, redirect } from 'react-router'
+import type { Route } from './+types/_home.user.group.$id_.post'
 
-const PostPage = () => {
-	return <div>PostPage</div>
+export default function PostPage() {
+	return (
+		<div>
+			<h2>új esemény létrehozása</h2>
+			<Form method='POST'>
+				<input type='hidden' name='groupId' value='1' />
+				<input type='text' name='title' id='title' placeholder='esemény neve' />
+				<input type='date' name='startdate' id='startdate' />
+				<input type='time' name='starttime' id='' />
+				<p>esemény plakátja</p>
+				<input type='file' name='attachment' accept='image/jpeg' required />
+				<br />
+				<input type='submit' value='letrehozás' />
+			</Form>
+		</div>
+	)
 }
 
-export default PostPage
+export const action = async ({ request }: Route.ActionArgs) => {
+	const formData = await request.formData()
+
+	return redirect('/user/group/1')
+}
