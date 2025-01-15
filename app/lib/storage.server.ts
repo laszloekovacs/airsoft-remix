2aa52565aa836eebcdaea2341fbb3e039ae6e418
laszloekovacs <laszloekovacs@gmail.com>
2025-01-15 11:18:12 +0100

    fixed

--- app/lib/storage.server.ts
+++ app/lib/storage.server.ts
@@ -2,6 +2,6 @@ export const storage_write = async (key: string, attachment: File) => {
 	const content_basepath = './upload/content/'
 	const path = content_basepath + key
 
-	const bytes = await Bun.write(path, key)
+	const bytes = await Bun.write(path, attachment)
 	return bytes
 }
