ad6a51eb9d741accd94816c53da579009bb18b18
Laszlo E Kovacs <laszloekovacs@gmail.com>
2025-01-17 16:29:04 +0100

    feat: navigate back button

--- app/routes/dashboard.tsx
+++ app/routes/dashboard.tsx
@@ -1,8 +1,10 @@
-import { Outlet } from 'react-router'
+import { Outlet, useNavigate, useNavigation } from 'react-router'
 
 const Dashboard = () => {
+	const navigate = useNavigate()
 	return (
 		<div className='container mx-auto'>
+			<button onClick={() => navigate(-1)}>vissza</button>
 			<h2>Dashboard</h2>
 			<Outlet />
 		</div>
