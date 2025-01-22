52b3c816109d979a536fab8d4e81f3ccd9042453
laszloekovacs <laszloekovacs@gmail.com>
2025-01-22 15:24:22 +0100

    styling

--- app/routes/dashboard.tsx
+++ app/routes/dashboard.tsx
@@ -3,10 +3,17 @@ import { Outlet, useNavigate, useNavigation } from 'react-router'
 const Dashboard = () => {
 	const navigate = useNavigate()
 	return (
-		<div className='container mx-auto'>
-			<button onClick={() => navigate(-1)}>vissza</button>
-			<h2>Dashboard</h2>
+		<div className='container'>
+			<header>
+				<button onClick={() => navigate(-1)}>vissza</button>
+				<h2>Dashboard</h2>
+			</header>
+
 			<Outlet />
+
+			<div>
+				<p>Dashboard</p>
+			</div>
 		</div>
 	)
 }
