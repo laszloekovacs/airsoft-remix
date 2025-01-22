cdf89f08ed9b006910ca7be6876e482ae2ea59e4
laszloekovacs <laszloekovacs@gmail.com>
2025-01-22 11:41:41 +0100

    Add user profile page with database integration

---
+++ app/routes/_home.users.$userId._index.tsx
@@ -0,0 +1,33 @@
+import { drizzleClient } from '~/lib/db.server'
+import { user } from '~/schema/auth-schema'
+import type { Route } from './+types/_home.users.$userId._index'
+import { eq } from 'drizzle-orm'
+
+export const loader = async ({ params, request }: Route.LoaderArgs) => {
+	// find the user id in the database
+	const userData = await drizzleClient
+		.select()
+		.from(user)
+		.where(eq(user.id, params.userId))
+
+	if (!userData) {
+		throw new Response('User not found', { status: 404 })
+	}
+
+	return { user: userData[0] }
+}
+
+const UserProfilePage = ({ loaderData }: Route.ComponentProps) => {
+	const { user } = loaderData
+	return (
+		<div>
+			<h1>Felhasználó</h1>
+
+			<p>
+				<strong>Név:</strong> {user.name}
+			</p>
+		</div>
+	)
+}
+
+export default UserProfilePage
