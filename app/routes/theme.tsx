ce9da3e3c6c7db64aec620204e0c86c2bbc716b8
laszloekovacs <laszloekovacs@gmail.com>
2025-01-27 13:42:59 +0100

    feat: add theme styles and components for improved UI structure

---
+++ app/routes/theme.tsx
@@ -0,0 +1,13 @@
+import style from './theme.module.css'
+
+const theme = () => {
+	return (
+		<div>
+			<div className={style.box}>
+				<p>its a box</p>
+			</div>
+		</div>
+	)
+}
+
+export default theme
