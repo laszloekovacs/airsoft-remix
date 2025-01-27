c967d174cd0df4a2abb5c465200e5a85b6b7abbc
laszloekovacs <laszloekovacs@gmail.com>
2025-01-27 13:31:56 +0100

    refactor: remove Tailwind CSS and add custom resets and theme styles

--- app/root.tsx
+++ app/root.tsx
@@ -6,27 +6,15 @@ import {
 	Scripts,
 	ScrollRestoration
 } from 'react-router'
-
 import type { Route } from './+types/root'
-import stylesheet from './app.css?url'
+
+import resets from './css/resets.css?url'
+import theme from './css/theme.css?url'
 
 export const links: Route.LinksFunction = () => [
-	{
-		rel: 'stylesheet',
-		href: 'https://unpkg.com/open-props'
-	},
-	{ rel: 'preconnect', href: 'https://fonts.googleapis.com' },
-	{
-		rel: 'preconnect',
-		href: 'https://fonts.gstatic.com',
-		crossOrigin: 'anonymous'
-	},
-	{
-		rel: 'stylesheet',
-		href: 'https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap'
-	},
-	{ rel: 'stylesheet', href: stylesheet },
-	{ rel: 'icon', href: '/favicon.ico?v2' }
+	{ rel: 'icon', href: '/favicon.ico?v2' },
+	{ rel: 'stylesheet', href: resets },
+	{ rel: 'stylesheet', href: theme }
 ]
 
 export function Layout({ children }: { children: React.ReactNode }) {
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,13 @@
 import { reactRouter } from '@react-router/dev/vite'
 import autoprefixer from 'autoprefixer'
-import tailwindcss from 'tailwindcss'
+import customMediaQuery from 'postcss-custom-media'
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
-import customMediaQuery from 'postcss-custom-media'
 
 export default defineConfig({
 	css: {
 		postcss: {
-			plugins: [autoprefixer, customMediaQuery, tailwindcss]
+			plugins: [autoprefixer, customMediaQuery]
 		}
 	},
 	plugins: [reactRouter(), tsconfigPaths()],
