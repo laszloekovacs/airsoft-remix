57e02decdc0ee85f54e7884398325799d709caba
laszloekovacs <laszloekovacs@gmail.com>
2025-01-30 12:21:15 +0100

    added page parameter to index

--- app/routes/_home._index.tsx
+++ app/routes/_home._index.tsx
@@ -6,8 +6,28 @@ import { event as CalendarEvent } from '~/schema'
 export const loader = async ({ request }: Route.LoaderArgs) => {
 	// extract 'page' search param
 	const url = new URL(request.url)
+	const page = url.searchParams.get('page')
 
-	const events = await drizzleClient.select().from(CalendarEvent)
+	if (page) {
+		const pageParam = parseInt(page)
+
+		if (isNaN(pageParam) || pageParam < 1) {
+			throw new Error('Invalid page parameter')
+		}
+
+		const offset = (pageParam - 1) * 10
+		console.log(offset)
+
+		const events = await drizzleClient
+			.select()
+			.from(CalendarEvent)
+			.limit(10)
+			.offset(offset)
+
+		return { events }
+	}
+
+	const events = await drizzleClient.select().from(CalendarEvent).limit(10)
 
 	return { events }
 }
