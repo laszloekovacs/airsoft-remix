6ee0f3fb4e3fe5b50e96f2acdbcd1d6f7673a96e
laszloekovacs <laszloekovacs@gmail.com>
2025-02-04 13:17:09 +0100

    feat: inputs for title, date

--- app/routes/_home.event.($eventUrl).edit.tsx
+++ app/routes/_home.event.($eventUrl).edit.tsx
@@ -3,6 +3,8 @@ import { event } from '~/schema'
 import { auth } from '~/services/auth.server'
 import { drizzleClient } from '~/services/db.server'
 import type { Route } from './+types/_home.event.($eventUrl).edit'
+import { Form, useFetcher } from 'react-router'
+import styles from './_home.event.($eventId).edit.module.css'
 
 export const loader = async ({ params, request }: Route.ActionArgs) => {
 	const session = await auth.api.getSession({ headers: request.headers })
@@ -13,33 +15,81 @@ export const loader = async ({ params, request }: Route.ActionArgs) => {
 
 	// it's a new event.
 	if (!params.eventUrl) {
-		return { data: null }
+		return { eventData: null }
 	}
 
 	// load inital event data.
-	const data = await drizzleClient
+	const eventData = await drizzleClient
 		.select()
 		.from(event)
 		.where(eq(event.url, params.eventUrl))
 
-	if (!data || data.length === 0) {
+	if (!eventData || eventData.length === 0) {
 		throw new Response('Event not found', { status: 404 })
 	}
 
 	// check if user id is the sames as event creator
-	if (data[0].createdBy !== session.user.id) {
+	if (eventData[0].createdBy !== session.user.id) {
 		throw new Response('Forbidden, ez nem a te eseményed!', { status: 403 })
 	}
 
-	return { data: data[0] }
+	return { eventData: eventData[0] }
 }
 
 export default function EventEditPage({ loaderData }: Route.ComponentProps) {
-	const { data } = loaderData
+	const { eventData } = loaderData
+	const fetcher = useFetcher()
 
 	return (
 		<div>
-			<pre>{JSON.stringify(data, null, 1)}</pre>
+			<fetcher.Form className={styles.formContainer} method='POST'>
+				<div>
+					<input type='hidden' name='id' value={eventData?.id ?? ''} />
+					<output>{eventData?.url}</output>
+				</div>
+				<div>
+					<label htmlFor='title'>Esemény Címe:</label>
+					<input
+						type='text'
+						id='title'
+						name='title'
+						defaultValue={eventData?.title ?? ''}
+					/>
+				</div>
+
+				<div>
+					<label htmlFor='startDate'>Esemény Napja:</label>
+					<input
+						type='date'
+						id='startDate'
+						name='startDate'
+						defaultValue={eventData?.startDate}
+					/>
+				</div>
+
+				<div>
+					<label htmlFor='isPublished'>Nyilvános?:</label>
+					<input
+						type='checkbox'
+						id='isPublished'
+						name='isPublished'
+						defaultChecked={eventData?.isPublished ?? false}
+					/>
+				</div>
+
+				<input type='submit' value='Mentés' />
+			</fetcher.Form>
+
+			<pre>{JSON.stringify(eventData, null, 1)}</pre>
 		</div>
 	)
+}
+
+export const action = async ({ request, params }: Route.ActionArgs) => {
+	const formData = await request.formData()
+	const updateData = Object.fromEntries(formData)
+
+	console.log(updateData)
+
+	return {}
 }
