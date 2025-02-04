8648b6fa85f973313a871fbe41ea60ded586ef3d
laszloekovacs <laszloekovacs@gmail.com>
2025-02-04 14:21:20 +0100

    made inputs controlled

--- app/routes/_home.event.($eventUrl).edit.tsx
+++ app/routes/_home.event.($eventUrl).edit.tsx
@@ -1,10 +1,11 @@
 import { eq } from 'drizzle-orm'
+import { useState } from 'react'
+import { useFetcher } from 'react-router'
 import { event } from '~/schema'
 import { auth } from '~/services/auth.server'
 import { drizzleClient } from '~/services/db.server'
 import type { Route } from './+types/_home.event.($eventUrl).edit'
-import { Form, useFetcher } from 'react-router'
-import styles from './_home.event.($eventId).edit.module.css'
+import styles from './_home.event.($eventUrl).edit.module.css'
 
 export const loader = async ({ params, request }: Route.ActionArgs) => {
 	const session = await auth.api.getSession({ headers: request.headers })
@@ -15,7 +16,7 @@ export const loader = async ({ params, request }: Route.ActionArgs) => {
 
 	// it's a new event.
 	if (!params.eventUrl) {
-		return { eventData: null }
+		return { eventData: {} as typeof event.$inferSelect }
 	}
 
 	// load inital event data.
@@ -39,46 +40,75 @@ export const loader = async ({ params, request }: Route.ActionArgs) => {
 export default function EventEditPage({ loaderData }: Route.ComponentProps) {
 	const { eventData } = loaderData
 	const fetcher = useFetcher()
+	const [formData, setFormData] = useState(eventData)
+
+	const handleSubmit = async (event: React.MouseEvent<HTMLButtonElement>) => {
+		event.preventDefault()
+
+		await fetcher.submit(JSON.stringify(formData), {
+			method: 'post',
+			encType: 'application/json'
+		})
+	}
 
 	return (
-		<div>
-			<fetcher.Form className={styles.formContainer} method='POST'>
-				<div>
-					<input type='hidden' name='id' value={eventData?.id ?? ''} />
-					<output>{eventData?.url}</output>
-				</div>
-				<div>
-					<label htmlFor='title'>Esemény Címe:</label>
-					<input
-						type='text'
-						id='title'
-						name='title'
-						defaultValue={eventData?.title ?? ''}
-					/>
-				</div>
+		<div className={styles.formContainer}>
+			<div>
+				<label htmlFor='id'>esemény azonosítószáma</label>
+				<input type='hidden' name='id' defaultValue={formData.id} />
+				<output id='id'>{formData.id}</output>
+			</div>
 
-				<div>
-					<label htmlFor='startDate'>Esemény Napja:</label>
-					<input
-						type='date'
-						id='startDate'
-						name='startDate'
-						defaultValue={eventData?.startDate}
-					/>
-				</div>
+			<div>
+				<label htmlFor='url'>Esemény URL:</label>
+				<output id='url'>{formData.url}</output>
+			</div>
 
-				<div>
-					<label htmlFor='isPublished'>Nyilvános?:</label>
-					<input
-						type='checkbox'
-						id='isPublished'
-						name='isPublished'
-						defaultChecked={eventData?.isPublished ?? false}
-					/>
-				</div>
+			<div>
+				<label htmlFor='title'>Esemény Címe:</label>
+				<input
+					type='text'
+					id='title'
+					name='title'
+					defaultValue={formData.title}
+					value={formData.title}
+					onChange={e => {
+						setFormData({ ...formData, title: e.target.value })
+					}}
+				/>
+			</div>
 
-				<input type='submit' value='Mentés' />
-			</fetcher.Form>
+			<div>
+				<label htmlFor='startDate'>Esemény Napja:</label>
+				<input
+					type='date'
+					id='startDate'
+					name='startDate'
+					defaultValue={formData.startDate}
+					value={formData.startDate}
+					onChange={e => {
+						setFormData({ ...formData, startDate: e.target.value })
+					}}
+				/>
+			</div>
+
+			<div>
+				<label htmlFor='isPublished'>Nyilvános?:</label>
+				<input
+					type='checkbox'
+					id='isPublished'
+					name='isPublished'
+					defaultChecked={formData.isPublished ?? false}
+					value={formData.isPublished ? 'on' : 'off'}
+					onChange={e => {
+						setFormData({ ...formData, isPublished: e.target.checked })
+					}}
+				/>
+			</div>
+
+			<button className='btn' onClick={handleSubmit}>
+				mentés
+			</button>
 
 			<pre>{JSON.stringify(eventData, null, 1)}</pre>
 		</div>
@@ -86,10 +116,9 @@ export default function EventEditPage({ loaderData }: Route.ComponentProps) {
 }
 
 export const action = async ({ request, params }: Route.ActionArgs) => {
-	const formData = await request.formData()
-	const updateData = Object.fromEntries(formData)
+	const formData = await request.json()
 
-	console.log(updateData)
+	console.log(formData)
 
 	return {}
 }
