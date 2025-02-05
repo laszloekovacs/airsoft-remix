489cda7353b40637175674b4628f5dba0be198aa
laszloekovacs <laszloekovacs@gmail.com>
2025-02-05 15:45:00 +0100

    more examples

--- app/routes/mdx.tsx
+++ app/routes/mdx.tsx
@@ -1,11 +1,17 @@
-import React, { useEffect, useRef, useState } from 'react'
-import '@mdxeditor/editor/style.css'
 import {
+	BoldItalicUnderlineToggles,
+	InsertImage,
 	MDXEditor,
+	UndoRedo,
 	headingsPlugin,
+	imagePlugin,
+	listsPlugin,
 	markdownShortcutPlugin,
+	toolbarPlugin,
 	type MDXEditorMethods
 } from '@mdxeditor/editor'
+import '@mdxeditor/editor/style.css'
+import { useRef } from 'react'
 import NoSSR from '~/components/nossr'
 
 export default function MdxPage() {
@@ -18,14 +24,36 @@ export default function MdxPage() {
 
 const Editor = () => {
 	const ref = useRef<MDXEditorMethods>(null)
+	const markdown = `
+Image without dimensions:
+![](https://picsum.photos/200/300)
+Image with dimensions:
+<img src="https://picsum.photos/200/300" width="100" height="150" />
+`
 
 	return (
 		<div>
 			<button onClick={() => ref.current?.setMarkdown('hello')}>tbn</button>
 			<MDXEditor
 				ref={ref}
-				markdown='bye'
-				plugins={[headingsPlugin(), markdownShortcutPlugin()]}
+				markdown={markdown}
+				plugins={[
+					headingsPlugin(),
+					listsPlugin(),
+					imagePlugin(),
+
+					markdownShortcutPlugin(),
+					toolbarPlugin({
+						toolbarClassName: 'toolbar',
+						toolbarContents: () => (
+							<>
+								<UndoRedo />
+								<BoldItalicUnderlineToggles />
+								<InsertImage />
+							</>
+						)
+					})
+				]}
 			/>
 		</div>
 	)
---
+++ app/routes/tiptap.tsx
@@ -0,0 +1,43 @@
+import NoSSR from '~/components/nossr'
+import StarterKit from '@tiptap/starter-kit'
+import Image from '@tiptap/extension-image'
+import {
+	useEditor,
+	EditorContent,
+	FloatingMenu,
+	BubbleMenu
+} from '@tiptap/react'
+
+const markdown = `
+Image without dimensions:
+![](https://picsum.photos/200/300)
+Image with dimensions:
+<img src="https://picsum.photos/200/300" width="100" height="150" />
+`
+
+export default function RawDataPage() {
+	return <Editor />
+}
+
+export const Editor = () => {
+	const editor = useEditor({
+		extensions: [
+			Image,
+			StarterKit.configure({
+				heading: {
+					levels: [1, 2]
+				}
+			})
+		],
+		content: markdown,
+		immediatelyRender: false
+	})
+
+	return (
+		<NoSSR>
+			<EditorContent editor={editor} />
+			<FloatingMenu editor={editor}>floating menu</FloatingMenu>
+			<BubbleMenu editor={editor}>This is the bubble menu</BubbleMenu>
+		</NoSSR>
+	)
+}
