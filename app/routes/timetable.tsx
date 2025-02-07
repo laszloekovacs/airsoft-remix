3d58368f4a1e5f64cc1e5e171700e33c07374f90
laszloekovacs <laszloekovacs@gmail.com>
2025-02-07 19:08:52 +0100

    added time table component

---
+++ app/routes/timetable.tsx
@@ -0,0 +1,52 @@
+import { useState } from 'react'
+
+export default function TimeTable() {
+	const [task, setTask] = useState<
+		Array<{
+			id: number
+			time: string
+			description: string
+		}>
+	>([])
+
+	const handleSubmit = (e: React.SyntheticEvent<HTMLFormElement>) => {
+		e.preventDefault()
+
+		const formData = new FormData(e.currentTarget)
+		const time = formData.get('time') as string
+		const description = formData.get('description') as string
+
+		if (time && description) {
+			setTask([...task, { id: Date.now(), time, description }])
+		}
+	}
+
+	const handleRemove = (id: number) => {
+		setTask(task.filter(t => t.id !== id))
+	}
+
+	return (
+		<div>
+			<h1>TimeTable</h1>
+
+			<form onSubmit={handleSubmit}>
+				<label htmlFor='time'>Időpont</label>
+				<input type='time' name='time' id='time' />
+				<br />
+				<label htmlFor='description'>Leírás</label>
+				<input type='text' name='description' id='description' />
+
+				<button>hozzáad</button>
+			</form>
+
+			<ul>
+				{task.map(t => (
+					<li key={t.id}>
+						{t.time} - {t.description}
+						<button onClick={() => handleRemove(t.id)}>x</button>
+					</li>
+				))}
+			</ul>
+		</div>
+	)
+}
