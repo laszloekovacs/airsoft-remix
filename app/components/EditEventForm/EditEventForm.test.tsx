535f9f96e8877325a6bd567c79034075954368a1
Laszlo E Kovacs <laszloekovacs@gmail.com>
2025-02-18 18:58:03 +0100

    renders form

--- app/components/EditEventForm/EditEventForm.test.tsx
+++ app/components/EditEventForm/EditEventForm.test.tsx
@@ -1,9 +1,14 @@
-import { describe, expect, it } from 'vitest'
-import { render, screen } from '@testing-library/react'
+import { beforeEach, describe, expect, it } from 'vitest'
+import { cleanup, render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom/vitest'
 
 describe('EditEventForm', () => {
+	beforeEach(() => {
+		cleanup()
+	})
+
 	it('should render a form', () => {
-		render(<p>test</p>)
-		expect(screen.getByRole('form')).toBeInTheDocument()
+		render(<form data-testid='form'>test</form>)
+		expect(screen.getByTestId('form')).toBeInTheDocument()
 	})
 })
