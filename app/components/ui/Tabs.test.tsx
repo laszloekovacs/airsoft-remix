3f03201557b1c40afc07d5380bd3e9ad2d0d1589
Laszlo E Kovacs <laszloekovacs@gmail.com>
2025-02-19 17:40:56 +0100

    fixed typo

--- app/components/ui/Tabs.test.tsx
+++ app/components/ui/Tabs.test.tsx
@@ -1,7 +1,7 @@
-import { describe, expect, it } from 'vitest'
-import { TabContainer, TabList, TabPanel, Tabs, TabTrigger } from './Tabs'
-import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom/vitest'
+import { cleanup, render, screen } from '@testing-library/react'
+import { afterEach, beforeEach, describe, expect, it } from 'vitest'
+import { TabContainer, TabList, TabPanel, Tabs, TabTrigger } from './Tabs'
 
 const Elements = () => (
 	<TabContainer>
@@ -19,9 +19,15 @@ const Elements = () => (
 )
 
 describe('Tab ui', () => {
-	const elements = it('should render divs and content', () => {
+	beforeEach(() => {
 		render(<Elements />)
+	})
 
+	afterEach(() => {
+		cleanup()
+	})
+
+	it('should render divs and content', () => {
 		expect(screen.getByText('Tab 1')).toBeInTheDocument()
 		expect(screen.getByText('Panel 2')).toBeInTheDocument()
 	})
