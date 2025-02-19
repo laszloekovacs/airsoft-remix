7ad2541d12acba9a8e31507fdd14c94519923ad9
laszloekovacs <laszloekovacs@gmail.com>
2025-02-19 09:07:10 +0100

    renders input for event name

--- app/components/EditEventForm/EditEventForm.tsx
+++ app/components/EditEventForm/EditEventForm.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react'
+
 type EventModel = {
 	name: string
 	url: string
@@ -8,5 +10,19 @@ type FormProps = {
 }
 
 export function EditEventForm({ intitalValues }: FormProps) {
-	return <form data-testid='form'>EditEventForm</form>
+	const [formState, setFormState] = useState(intitalValues)
+
+	return (
+		<form data-testid='form'>
+			<label>
+				<span>Esemény neve</span>
+				<input
+					type='text'
+					name='name'
+					value={formState.name}
+					onChange={e => setFormState({ ...formState, name: e.target.value })}
+				/>
+			</label>
+		</form>
+	)
 }
--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,13 @@ import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-	plugins: [reactRouter(), tsconfigPaths()],
+	plugins: [!process.env.VITEST && reactRouter(), tsconfigPaths()],
 	server: {
 		port: 3000,
 		host: '0.0.0.0'
 	},
 	test: {
+		globals: true,
 		environment: 'happy-dom'
 	}
 })
