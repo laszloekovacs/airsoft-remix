1c37433f46da02711d3fbe04dfdb9277aba8d71c
Laszlo E Kovacs <laszloekovacs@gmail.com>
2025-02-21 15:09:15 +0100

    moved viewModel to its own file

--- app/queries/queryEventList.server.ts
+++ app/queries/queryEventList.server.ts
@@ -2,9 +2,18 @@
 
 import { drizzleClient } from '~/services/db.server'
 import { event } from '~/schema'
+import type { EventListItem } from '~/components/EventList/EventList'
 
 export const queryEventList = async () => {
 	const eventList = await drizzleClient.select().from(event)
 
-	return eventList
+	const eventViewModel: EventListItem[] = eventList.map(e => ({
+		id: e.id,
+		title: e.title,
+		url: e.url,
+		createdBy: e.createdBy || 'anon',
+		startDate: e.startDate,
+		coverPhoto: e.coverPhoto || 'https://picsum.photos/820/312'
+	}))
+	return eventViewModel
 }
--- app/routes/_home._index.tsx
+++ app/routes/_home._index.tsx
@@ -1,28 +1,18 @@
-import { EventList, type EventListItem } from '~/components/EventList/EventList'
+import { EventList } from '~/components/EventList/EventList'
 import { queryEventList } from '~/queries/queryEventList.server'
 import type { Route } from './+types/_home._index'
 
 export const loader = async ({ request }: Route.LoaderArgs) => {
 	const eventList = await queryEventList()
 
-	const events: EventListItem[] = eventList.map(e => ({
-		id: e.id,
-		title: e.title,
-		url: e.url,
-		createdBy: e.createdBy || 'anon',
-		startDate: e.startDate,
-		coverPhoto: e.coverPhoto || 'https://picsum.photos/820/312'
-	}))
-
-	return { events }
+	return { eventList }
 }
 export default function HomeIndex({ loaderData }: Route.ComponentProps) {
-	const { events } = loaderData
-
+	const { eventList } = loaderData
 	return (
 		<div>
 			<h2>Játékok</h2>
-			<EventList events={events} />
+			<EventList events={eventList} />
 		</div>
 	)
 }
