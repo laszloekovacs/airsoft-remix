bd23f86537092ec13cc97c84b16e1363f678f4ff
Laszlo E Kovacs <laszloekovacs@gmail.com>
2025-02-21 17:56:24 +0100

    replaced user event table

---
+++ app/queries/queryUserEventList.server.ts
@@ -0,0 +1,20 @@
+import { eq } from 'drizzle-orm'
+import type { UserEventTableRow } from '~/components/UsersEventTable/UserEventTable'
+import { event } from '~/schema'
+import { drizzleClient } from '~/services/db.server'
+
+export const queryUserEventList = async (userId: string) => {
+	const databaseEvents = await drizzleClient
+		.select()
+		.from(event)
+		.where(eq(event.createdBy, userId))
+
+	const eventViewModel: UserEventTableRow[] = databaseEvents.map(e => ({
+		id: e.id,
+		title: e.title,
+		url: e.url,
+		startDate: e.startDate
+	}))
+
+	return eventViewModel
+}
--- app/routes/_home.user._index.tsx
+++ app/routes/_home.user._index.tsx
@@ -1,9 +1,7 @@
-import { eq } from 'drizzle-orm'
 import ContactList from '~/components/contact-list'
-import UserCreatedEventsTable from '~/components/user-created-events-table'
-import { event as CalendarEvent } from '~/schema'
+import UserEventTable from '~/components/UsersEventTable/UserEventTable'
+import { queryUserEventList } from '~/queries/queryUserEventList.server'
 import { getSession } from '~/services/auth.server'
-import { drizzleClient } from '~/services/db.server'
 import type { Route } from './+types/_home.user._index'
 
 export const loader = async ({ request }: Route.LoaderArgs) => {
@@ -12,22 +10,19 @@ export const loader = async ({ request }: Route.LoaderArgs) => {
 	if (!session || !session?.user)
 		return new Response('Unauthorized', { status: 401 })
 
-	const events = await drizzleClient
-		.select()
-		.from(CalendarEvent)
-		.where(eq(CalendarEvent.createdBy, session.user.id))
+	const userEvents = await queryUserEventList(session.user.id)
 
-	return { events }
+	return { userEvents }
 }
 
 export default function UserIndexPage({ loaderData }: Route.ComponentProps) {
-	const { events } = loaderData
+	const { userEvents } = loaderData
 
 	return (
 		<div>
 			<ContactList contacts={[]} />
 
-			<UserCreatedEventsTable events={events} />
+			<UserEventTable eventSummaryList={userEvents} />
 		</div>
 	)
 }
