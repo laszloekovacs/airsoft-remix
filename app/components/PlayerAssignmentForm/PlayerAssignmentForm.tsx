e5ae3c5644f76d14c7867e9a6f027ae3c2c1e194
Laszlo E Kovacs <laszloekovacs@gmail.com>
2025-02-22 15:35:41 +0100

    prototyped player assingment list

--- app/components/PlayerAssignmentForm/PlayerAssignmentForm.tsx
+++ app/components/PlayerAssignmentForm/PlayerAssignmentForm.tsx
@@ -1,6 +1,6 @@
-import { useState } from 'react'
+import { useMemo, useState } from 'react'
 
-type PlayerAssignment = {
+type PlayerInfo = {
 	id: string
 	name: string
 	callsign: string
@@ -9,17 +9,68 @@ type PlayerAssignment = {
 }
 
 type PlayerAssignmentFormProps = {
-	players: PlayerAssignment[]
+	eventId: string
+	players: PlayerInfo[]
 }
+export const PlayerAssignmentForm = (props: PlayerAssignmentFormProps) => {
+	const { players: playerList } = props
+	const { eventId } = props
 
-export default function PlayerAssignmentForm(props: PlayerAssignmentFormProps) {
-	const [playerList, setPlayerList] = useState(props.players)
-
-	const factions = Object.groupBy(playerList, p => p.faction)
+	const factions = useMemo(
+		() => Object.groupBy(playerList, p => p.faction),
+		[playerList]
+	)
 
 	return (
-		<div data-testid='player-assignment-form'>
-			<pre>{JSON.stringify(factions, null, 2)}</pre>
+		<div id={eventId}>
+			<ul>
+				{Object.keys(factions).map(faction => {
+					return (
+						<Faction
+							key={faction}
+							faction={faction}
+							players={factions[faction]}
+						/>
+					)
+				})}
+			</ul>
 		</div>
+	)
+}
+
+const Faction = (props: { faction: string; players?: PlayerInfo[] }) => {
+	const { faction, players } = props
+
+	if (!players)
+		return (
+			<li id={faction}>
+				<h3>{faction}</h3>
+				<p>No players assigned</p>
+			</li>
+		)
+
+	return (
+		<li id={faction}>
+			<h3>{faction}</h3>
+			<ul>
+				{players?.map(player => (
+					<PlayerCard key={player.id} {...player} />
+				))}
+			</ul>
+		</li>
+	)
+}
+
+const PlayerCard = ({ ...props }: PlayerInfo) => {
+	const { id, name, callsign, avatar } = props
+
+	return (
+		<li id={callsign}>
+			<div>
+				<h3>{callsign}</h3>
+				<h2>{name}</h2>
+				<img src={avatar ?? null} alt={name} />
+			</div>
+		</li>
 	)
 }
--- app/routes/status.tsx
+++ app/routes/status.tsx
@@ -1,43 +1,29 @@
-import { useState, type DragEventHandler } from 'react'
+import { PlayerAssignmentForm } from '~/components/PlayerAssignmentForm/PlayerAssignmentForm'
 
 export default function Status() {
-	const handleDragStart = (
-		e: React.DragEvent<HTMLDivElement>,
-		data: string
-	) => {
-		e.dataTransfer.setData('text/plain', data)
-		e.dataTransfer.effectAllowed = 'move'
+	const players = [
+		{
+			id: '1',
+			name: 'Adam',
+			callsign: 'Adam',
+			avatar: 'https://picsum.photos/50',
+			faction: 'blue'
+		},
+		{
+			id: '2',
+			name: 'Bravo',
+			callsign: 'Bravo',
+			avatar: 'https://picsum.photos/50',
+			faction: 'red'
+		},
+		{
+			id: '3',
+			name: 'charlie',
+			callsign: 'Charlie',
+			avatar: 'https://picsum.photos/50',
+			faction: 'blue'
+		}
+	]
 
-		console.log('grabbed', e.dataTransfer.getData('text/plain'))
-	}
-
-	const handleDrop = (e: React.DragEvent<HTMLDivElement>) => {
-		e.preventDefault()
-
-		const data = e.dataTransfer.getData('text/plain')
-
-		console.log('dropped', data)
-	}
-
-	const handleDragEnd = (e: React.DragEvent<HTMLDivElement>) => {
-		console.log('drag end', e.dataTransfer.getData('text/plain'))
-	}
-
-	return (
-		<div>
-			<div
-				draggable
-				onDragStart={e => handleDragStart(e, '1')}
-				onDragEnd={handleDragEnd}>
-				drag me
-			</div>
-
-			<div
-				className='bg-amber-300 w-16 h-16'
-				onDragOver={e => e.preventDefault()}
-				onDrop={handleDrop}>
-				drop here
-			</div>
-		</div>
-	)
+	return <PlayerAssignmentForm players={players} />
 }
