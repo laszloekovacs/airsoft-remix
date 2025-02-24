7db35ace06e52af7a86b7191ed0bd3e68ca5f755
laszloekovacs <laszloekovacs@gmail.com>
2025-02-24 11:17:29 +0100

    removed faulty stub

--- app/routes/_home.event.($eventUrl).edit.tsx
+++ app/routes/_home.event.($eventUrl).edit.tsx
@@ -1,11 +1,13 @@
 import type { Route } from './+types/_home.event.($eventUrl).edit'
 
-const loader = async ({ params, request }: Route.ActionArgs) => {
-
+export const loader = async ({ params, request }: Route.ActionArgs) => {
+	return {
+		user: "mike"
+	}
 }
 
 export default function EventEditPage({ loaderData }: Route.ComponentProps) {
-
+	const user = loaderData.user
 
 	return (
 		<div>
