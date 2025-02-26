6517f3a0b513d882e487cbd3ee834d7a3c29f85f
laszloekovacs <laszloekovacs@gmail.com>
2025-02-26 09:45:46 +0100

    implemented saving

--- app/routes/_home.event.($eventUrl).edit._index.tsx
+++ app/routes/_home.event.($eventUrl).edit._index.tsx
@@ -1,13 +1,14 @@
 import { eq } from 'drizzle-orm'
 import { useDeferredValue, useEffect, useState } from 'react'
-import { useFetcher } from 'react-router'
-import { generateUrlName } from '~/helpers/generate-url-name'
+import { redirect, useFetcher } from 'react-router'
+import { generateUrlName as generateUrlSafeName } from '~/helpers/generate-url-name'
 import { event } from '~/schema'
 import { getSession } from '~/services/auth.server'
 import { drizzleClient } from '~/services/db.server'
 import type { Route } from './+types/_home.event.($eventUrl).edit._index'
 
 type LoaderDataViewModel = {
+	id: string
 	url: string
 	title: string
 	startDate: string
@@ -17,8 +18,6 @@ export const loader = async ({ params, request }: Route.LoaderArgs) => {
 	const sessionCookie = await getSession(request)
 	if (!sessionCookie) throw new Response('Unauthorized', { status: 401 })
 
-	// TODO: check if its the users event
-	// use database index to identify event
 	let result
 
 	if (params.eventUrl) {
@@ -26,17 +25,27 @@ export const loader = async ({ params, request }: Route.LoaderArgs) => {
 			.select()
 			.from(event)
 			.where(eq(event.url, params.eventUrl))
+
+		if (result.length == 0) {
+			throw new Response('not found', { status: 404 })
+		}
+
+		if (result.length > 1) {
+			throw new Response('url not unique', { status: 500 })
+		}
 	}
 
 	const loaderData: LoaderDataViewModel = {
+		id: result?.[0].id || '',
 		url: params.eventUrl || '',
-		title: (result && result[0].title) || '',
-		startDate: (result && result[0].startDate) || ''
+		title: result?.[0].title || '',
+		startDate: result?.[0].startDate || ''
 	}
+
 	return loaderData
 }
 
-const GeneratedUrl = ({
+const PreviewUrl = ({
 	fetcher,
 	title,
 	startDate
@@ -47,7 +56,7 @@ const GeneratedUrl = ({
 }) => {
 	useEffect(() => {
 		fetcher.submit(
-			{ intent: 'verify_url', title, startDate },
+			{ intent: 'verify', title, startDate },
 			{ method: 'post', encType: 'application/json' }
 		)
 	}, [title, startDate])
@@ -58,7 +67,6 @@ const GeneratedUrl = ({
 export default function EventEditIndexPage({
 	loaderData
 }: Route.ComponentProps) {
-	const [url, setUrl] = useState(loaderData.url)
 	const [title, setTitle] = useState(loaderData.title)
 	const [startDate, setStartDate] = useState(loaderData.startDate)
 	const fetcher = useFetcher<typeof action>()
@@ -66,9 +74,35 @@ export default function EventEditIndexPage({
 	const deferredTitle = useDeferredValue(title)
 	const deferredStartDate = useDeferredValue(startDate)
 
+	const isPending = fetcher.state !== 'idle'
+
+	const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault()
+
+		if (isPending) return
+
+		const formData = {
+			id: loaderData.id,
+			title,
+			startDate,
+			intent: 'save'
+		}
+
+		// validate form
+		if (!title || !startDate) return
+		if (title.length < 3) return
+
+		await fetcher.submit(formData, {
+			method: 'post',
+			encType: 'application/json'
+		})
+	}
+
 	return (
-		<fetcher.Form method='post'>
+		<fetcher.Form method='post' onSubmit={handleSubmit}>
 			<label htmlFor='title'>Esemény neve</label>
+
+			<input type='hidden' name='id' value={loaderData.id} />
 
 			<input
 				id='title'
@@ -86,14 +120,21 @@ export default function EventEditIndexPage({
 				onChange={e => setStartDate(e.target.value)}
 			/>
 
-			<pre>{JSON.stringify(fetcher.state)}</pre>
-			<GeneratedUrl
+			<PreviewUrl
 				fetcher={fetcher}
 				title={deferredTitle}
 				startDate={deferredStartDate}
 			/>
+
+			<input type='submit' value='mentés' />
 		</fetcher.Form>
 	)
+}
+
+type ActionResultModel = {
+	id: string
+	url: string
+	isAvailable: boolean
 }
 
 export const action = async ({ request }: Route.ActionArgs) => {
@@ -101,15 +142,48 @@ export const action = async ({ request }: Route.ActionArgs) => {
 	if (!sessionCookie) throw new Response('Unauthorized', { status: 401 })
 
 	const formData = await request.json()
-	const { title, startDate } = formData
+	const { id, title, startDate, intent } = formData
 
-	const generatedUrl = startDate + '-' + generateUrlName(title)
+	const generatedUrl = startDate + '-' + generateUrlSafeName(title)
+	let isAvaliable = false
 
-	// check if it's not in use
-	const result = await drizzleClient
-		.select()
-		.from(event)
-		.where(eq(event.url, generatedUrl))
+	switch (intent) {
+		case 'save': {
+			const result = await drizzleClient
+				.update(event)
+				.set({
+					title,
+					startDate,
+					url: generatedUrl
+				})
+				.where(eq(event.id, id))
+				.returning({ id: event.id, url: event.url })
 
-	return { url: generatedUrl, urlInUse: result.length > 0 ? true : false }
+			return redirect(`/event/${result[0].url}/edit`)
+		}
+
+		case 'verify': {
+			// check if it's not in use
+			const result = await drizzleClient
+				.select()
+				.from(event)
+				.where(eq(event.url, generatedUrl))
+
+			// not found, or its the current event, safe to use
+			if (result.length == 0 || result[0].id == id) {
+				isAvaliable = true
+			}
+
+			const response: ActionResultModel = {
+				id,
+				url: generatedUrl,
+				isAvailable: isAvaliable
+			}
+
+			return response
+		}
+
+		default:
+			throw new Response('Bad intent', { status: 400 })
+	}
 }
