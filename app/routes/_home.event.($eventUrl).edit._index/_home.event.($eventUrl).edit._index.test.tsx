7fbb512ddc16ab5b9306750db83ab99e4d478cf5
laszloekovacs <laszloekovacs@gmail.com>
2025-02-26 11:19:13 +0100

    refactored handler into its own directory

--- app/routes/_home.event.($eventUrl).edit._index/_home.event.($eventUrl).edit._index.test.tsx
+++ app/routes/_home.event.($eventUrl).edit._index/_home.event.($eventUrl).edit._index.test.tsx
@@ -2,7 +2,7 @@ import '@testing-library/jest-dom/vitest'
 import { render, screen } from '@testing-library/react'
 import { createRoutesStub } from 'react-router'
 import { describe, expect, it } from 'vitest'
-import EventEditIndexPage from './_home.event.($eventUrl).edit._index'
+import EventEditIndexPage from '../_home.event.($eventUrl).edit._index'
 
 describe('Edit event index page', () => {
 	it('renders a form with a title input, fills in title', () => {
