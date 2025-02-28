97887bb696f3b42a2c8e86b9b93e66e2338a2ac3
laszloekovacs <laszloekovacs@gmail.com>
2025-02-28 19:04:42 +0100

    added display of title, start date

--- app/routes/_home.event.$eventUrl.apply/route.tsx
+++ app/routes/_home.event.$eventUrl.apply/route.tsx
@@ -1,18 +1,27 @@
 import type { Route } from '.react-router/types/app/routes/_home.event.$eventUrl.apply/+types/route'
+import type { User } from 'better-auth'
+import { eq } from 'drizzle-orm'
+import { Form } from 'react-router'
+import { event } from '~/schema'
 import { getSessionCookie, isSessionCookie } from '~/services/auth.server'
 import { db } from '~/services/db.server'
-import { event } from '~/schema'
-import { eq } from 'drizzle-orm'
 
 type ApplyToEventViewModel = {
 	id: string
 	title: string
+	startDate: string
+	coverPhoto: string | null
+	applicant: User
 }
 
 export const loader = async ({ params, request }: Route.LoaderArgs) => {
 	const sessionCookie = await getSessionCookie(request)
 
-	if (!isSessionCookie(sessionCookie)) {
+	if (!sessionCookie && !isSessionCookie(sessionCookie)) {
+		throw new Error('Unauthorized')
+	}
+
+	if (sessionCookie?.user === undefined) {
 		throw new Error('Unauthorized')
 	}
 
@@ -27,16 +36,27 @@ export const loader = async ({ params, request }: Route.LoaderArgs) => {
 
 	const applyToEventData: ApplyToEventViewModel = {
 		id: result[0].id,
-		title: result[0].title
+		title: result[0].title,
+		startDate: result[0].startDate,
+		coverPhoto: result[0].coverPhoto,
+		applicant: sessionCookie.user
 	}
 
-	return { applyToEventData }
+	return applyToEventData
 }
 
 export default function ApplyToEventPage({ loaderData }: Route.ComponentProps) {
+	const { title, applicant, startDate } = loaderData
+
 	return (
 		<div>
-			<p>Jelentkezés Eseményre</p>
+			<h2>Jelentkezés Eseményre</h2>
+			<h2>{title}</h2>
+			<p>{startDate}</p>
+			<Form>
+				<input type='submit' value='Submit' />
+				<input type='reset' value='Reset' />
+			</Form>
 		</div>
 	)
 }
