d9b530609f9fa67bbaf23ad12574b80bfc81c20a
laszloekovacs <laszloekovacs@gmail.com>
2025-02-28 15:16:26 +0100

    moved loader and action back into its place

--- app/routes/_home.event.($eventUrl).edit._index/route.tsx
+++ app/routes/_home.event.($eventUrl).edit._index/route.tsx
@@ -1,11 +1,104 @@
-import { useDeferredValue, useEffect, useState } from 'react'
-import { useFetcher } from 'react-router'
-import type { Route } from './+types/route'
-import { action } from './action'
-import { loader } from './loader'
+import { eq } from 'drizzle-orm'
+import { useDeferredValue, useState } from 'react'
+import { redirect, useFetcher } from 'react-router'
+import { getSession } from '~/services/auth.server'
+import { db } from '~/services/db.server'
 import { PreviewUrl } from './PreviewUrl'
+import { event } from '~/schema'
+import type { Route } from '../+types/_home.event.($eventUrl).edit'
 
-export { action, loader }
+export type LoaderDataViewModel = {
+	id: string
+	url: string
+	title: string
+	startDate: string
+}
+
+export const loader = async ({ params, request }: Route.LoaderArgs) => {
+	const sessionCookie = await getSession(request)
+	if (!sessionCookie) throw new Response('Unauthorized', { status: 401 })
+
+	let result
+
+	// event name parameter provided in the url
+	if (params.eventUrl) {
+		result = await db.select().from(event).where(eq(event.url, params.eventUrl))
+
+		if (result.length == 0) {
+			throw new Response('not found', { status: 404 })
+		}
+
+		if (result.length > 1) {
+			throw new Response('url not unique', { status: 500 })
+		}
+	}
+
+	const loaderData: LoaderDataViewModel = {
+		id: result?.[0].id || '',
+		url: result?.[0].url || '',
+		title: result?.[0].title || '',
+		startDate: result?.[0].startDate || ''
+	}
+
+	return loaderData
+}
+
+type ActionResultModel = {
+	id: string
+	url: string
+	isAvailable: boolean
+}
+
+export const action = async ({ request }: Route.ActionArgs) => {
+	const sessionCookie = await getSession(request)
+	if (!sessionCookie) throw new Response('Unauthorized', { status: 401 })
+
+	const formData = await request.json()
+	const { id, title, startDate, intent } = formData
+
+	const generatedUrl = startDate + '-' + generateUrlSafeName(title)
+	let isAvaliable = false
+
+	switch (intent) {
+		case 'save': {
+			const result = await db
+				.update(event)
+				.set({
+					title,
+					startDate,
+					url: generatedUrl
+				})
+				.where(eq(event.id, id))
+				.returning({ id: event.id, url: event.url })
+
+			return redirect(`/event/${result[0].url}/edit`)
+		}
+
+		case 'verify': {
+			// check if it's not in use
+			const result = await db
+				.select()
+				.from(event)
+				.where(eq(event.url, generatedUrl))
+
+			// not found, or its the current event, safe to use
+			if (result.length == 0 || result[0].id == id) {
+				isAvaliable = true
+			}
+
+			const response: ActionResultModel = {
+				id,
+				url: generatedUrl,
+				isAvailable: isAvaliable
+			}
+
+			return response
+		}
+
+		default:
+			throw new Response('Bad intent', { status: 400 })
+	}
+}
 
 export default function EventEditIndexPage({
 	loaderData
