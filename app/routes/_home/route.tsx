9f7afa26afa474564463f65e64c7017daf1b79d8
Laszlo E Kovacs <laszloekovacs@gmail.com>
2025-02-28 20:33:26 +0100

    moved footer to the home route

--- app/routes/_home/route.tsx
+++ app/routes/_home/route.tsx
@@ -1,10 +1,8 @@
-import { Container, Flex, Grid } from '@radix-ui/themes'
 import { Outlet } from 'react-router'
-import { HomeFooter } from '~/components/home-footer'
+import { HomeFooter } from '~/routes/_home/HomeFooter'
 import { PageLogo } from '~/components/page-logo'
 import SessionHeader from '~/components/session-header'
-import { auth, getCookieFromRequest } from '~/services/auth.server'
-import type { Route } from './+types/_home'
+import { getCookieFromRequest } from '~/services/auth.server'
 
 export function meta({}: Route.MetaArgs) {
 	return [
@@ -14,15 +12,15 @@ export function meta({}: Route.MetaArgs) {
 		{ property: 'type', content: 'website' },
 		{ property: 'og:title', content: 'Airsoft Naptár' },
 		{ property: 'og:description', content: 'Airsoft esemény naptár' },
+		{ property: 'og:site_name', content: 'Airsoft Naptár' },
 		{
 			property: 'og:image',
 			content: 'og-image.jpg'
-		},
-		{ property: 'og:site_name', content: 'Airsoft Naptár' }
+		}
 	]
 }
 
-export async function loader({ params, request }: Route.LoaderArgs) {
+export async function loader({ request }: Route.LoaderArgs) {
 	// returns email, if logged in aka session is not null
 	const session = await getCookieFromRequest(request)
 
@@ -33,24 +31,17 @@ export default function HomePage({ loaderData }: Route.ComponentProps) {
 	const sessionData = loaderData
 
 	return (
-		<Container maxWidth='800px'>
-			<Grid
-				p='2'
-				py='4'
-				columns='1'
-				gap='4'
-				rows='auto 1fr auto'
-				width='auto'
-				minHeight='100vh'>
-				<Flex justify='between' align='baseline'>
+		<div>
+			<div>
+				<div>
 					<PageLogo />
 					<SessionHeader sessionData={sessionData} />
-				</Flex>
+				</div>
 
 				<Outlet />
 
 				<HomeFooter />
-			</Grid>
-		</Container>
+			</div>
+		</div>
 	)
 }
