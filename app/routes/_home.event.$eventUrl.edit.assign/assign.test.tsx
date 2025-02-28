06bc18dd738abc5da34bb77e863d954dea79542d
laszloekovacs <laszloekovacs@gmail.com>
2025-02-28 19:33:25 +0100

    added test cases

--- app/routes/_home.event.$eventUrl.edit.assign/assign.test.tsx
+++ app/routes/_home.event.$eventUrl.edit.assign/assign.test.tsx
@@ -2,4 +2,6 @@ import { describe, it } from 'vitest'
 
 describe('player assignment page', () => {
 	it.todo('renders itself', () => {})
+	it.todo('renders a list of factions', () => {})
+	it.todo('renders the unasigned faction', () => {})
 })
