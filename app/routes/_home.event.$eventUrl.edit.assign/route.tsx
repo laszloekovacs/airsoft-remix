c6be9805709ceef5cfa27dc43f4014256667697a
laszloekovacs <laszloekovacs@gmail.com>
2025-02-28 19:14:25 +0100

    scaffolded assign players page

---
+++ app/routes/_home.event.$eventUrl.edit.assign/route.tsx
@@ -0,0 +1,9 @@
+import type { Route } from '.react-router/types/app/routes/_home.event.$eventUrl.edit.assign/+types/route'
+
+export const loader = ({ params }: Route.LoaderArgs) => {
+	return {}
+}
+
+export default function AssignPlayersPage({
+	loaderData
+}: Route.ComponentProps) {}
