9a2a6597d6a945faba061f5acf10d027db72c5fe
laszloekovacs <laszloekovacs@gmail.com>
2025-03-04 12:40:27 +0100

    added mock functions

--- app/routes/_home.event.($eventUrl).edit._index/PreviewUrl.test.tsx
+++ app/routes/_home.event.($eventUrl).edit._index/PreviewUrl.test.tsx
@@ -1,5 +1,21 @@
-import { expect, it } from 'vitest'
+import { beforeAll, describe, expect, it, vi } from 'vitest'
 
-it('should render its own div container', () => {
-	expect(true).toBe(true)
+describe('PreviewUrl', () => {
+	beforeAll(() => {})
+
+	it('should render its own div container', () => {
+		expect(true).toBe(true)
+	})
+
+	it('fetches the url and displays it', () => {
+		vi.mock('react-router', () => ({
+			fetcher: {
+				submit: vi.fn(),
+				data: {
+					url: 'mike',
+					isAvailable: true
+				}
+			}
+		}))
+	})
 })
